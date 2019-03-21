276ccc00335e656d704f65157511f71c72787c9c
iangifford <lightyagamie10@gmail.com>
2019-03-21 10:41:14 -0700

    adds the challenge start and tests

---
+++ Code-Challenges/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinarySearch();
+        }
+        public static int BinarySearch(int[] key)
+        {
+
+        }
+    }
+}
