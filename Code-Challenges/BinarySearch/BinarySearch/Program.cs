7e63b8ec1bc2c695760c12ae136881d4ad67761f
iangifford <lightyagamie10@gmail.com>
2019-03-21 11:08:30 -0700

    building logic

--- Code-Challenges/BinarySearch/BinarySearch/Program.cs
+++ Code-Challenges/BinarySearch/BinarySearch/Program.cs
@@ -8,9 +8,31 @@ namespace BinarySearch
         {
             BinarySearch();
         }
-        public static int BinarySearch(int[] key)
-        {
 
+        public static int BinarySearch(int[] binary, int key)
+        {
+           int mP = binary.Length / 2;
+            int lft = 0;
+            int rgt = binary.Length;
+            while (lft <= rgt)
+	        {
+
+                if(binary[mP] == key)
+                {
+                    return mP;
+                } else if (binary[mP] < key)
+                {
+                    lft = mP;
+                    mP = ((rgt + lft) / 2);
+
+                } else if (binary[mP] > key)
+                {
+                    rgt = mP;
+                    mP = ((lft + rgt) / 2);
+                }
+            
+	        }
+            return -1;
         }
     }
 }
