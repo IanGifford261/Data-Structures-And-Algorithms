b17552c8a88f9f9bf08542183cf2bcf1e4983dba
iangifford <lightyagamie10@gmail.com>
2019-04-03 09:30:55 -0700

    Switched to new branch

--- Code-Challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
+++ Code-Challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FIFOAnimalShelter.Classes;
 
 namespace FIFOAnimalShelter.Classes
 {
-    class AnimalShelter
+    public class AnimalShelter
     {
     }
 }
