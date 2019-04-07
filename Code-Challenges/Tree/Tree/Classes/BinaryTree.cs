d22ab04f42ca80af9ca4a359df8b7aab415c40b0
iangifford <lightyagamie10@gmail.com>
2019-04-07 13:28:12 -0700

    Preorder and recursory build

--- Code-Challenges/Tree/Tree/Classes/BinaryTree.cs
+++ Code-Challenges/Tree/Tree/Classes/BinaryTree.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Tree.Classes
 {
     public class BinaryTree
     {
         public static Node Root { get; set; }
-
+        public static List<int> ValueList = new List<int>();
         //Pre
+        public static int[] PreOrder()
+        {
+            if(ValueList.Count > 0)
+            {
+                ValueList.Clear();
+            
+            }
+
+            PreorderRecurs(Root);
+
+        }
+
+        public static void PreorderRecurs(Node node)
+        {
+            ValueList.Add.node.
+        }
+
         //In
+        public static int[] InOrder()
+        {
+
+        }
+
         //Post
-        
+        public static int[] PostOrder()
+        {
+
+        }
+
     }
 }
