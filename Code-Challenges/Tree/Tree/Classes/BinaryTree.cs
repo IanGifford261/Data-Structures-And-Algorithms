232d5fe56bc8e5868f81919d4c3e9faccd85f6e1
iangifford <lightyagamie10@gmail.com>
2019-04-07 14:22:03 -0700

    InOrder/Recurs and PostOrder/Recurse built

--- Code-Challenges/Tree/Tree/Classes/BinaryTree.cs
+++ Code-Challenges/Tree/Tree/Classes/BinaryTree.cs
@@ -46,14 +46,74 @@ namespace Tree.Classes
         //In
         public static int[] InOrder()
         {
+            if (ValueList.Count > 0)
+            {
+                ValueList.Clear();
+            }
+
+            InOrderRecurs(Root);
+            int[] arr = new int[ValueList.Count];
+            int i = 0;
+            foreach (var item in ValueList)
+            {
+                arr[i] = item;
+                i++;
+            }
+            return arr;
+
+        }
+
+        public static void InOrderRecurs(Node node)
+        {
+            if (node.Left != null)
+            {
+                InOrderRecurs(node.Left);
+            }
+
+            ValueList.Add(node.Value);
+
+            if (node.Right != null)
+            {
+                InOrderRecurs(node.Right);
+            }
 
         }
 
         //Post
         public static int[] PostOrder()
         {
+            if (ValueList.Count > 0)
+            {
+                ValueList.Clear();
+            }
+
+            PostOrderRecurs(Root);
+            int[] arr = new int[ValueList.Count];
+            int i = 0;
+            foreach (var item in ValueList)
+            {
+                arr[i] = item;
+                i++;
+            }
+            return arr;
 
         }
+
+        public static void PostOrderRecurs(Node node)
+        {
+            if (node.Left != null)
+            {
+                PostOrderRecurs(node.Left);
+            }
+            if (node.Right != null)
+            {
+                PostOrderRecurs(node.Right);
+            }
+
+            ValueList.Add(node.Value);
+        }
+
+    }
 
     }
 }
