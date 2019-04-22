3e036d8842c8e4375828d713b33201a4ace4f39f
iangifford <lightyagamie10@gmail.com>
2019-04-22 10:05:09 -0700

    Build out psuedo code from the reading.

---
+++ Code-Challenges/sorts/sorts/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sorts
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int j = i - 1;
+                int temp = arr[i];
+
+                while (j >= 0 && temp < arr[i])
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+
+                }
+                arr[j + 1] = temp;
+            }
+            return arr;
+        }
+    }
+}
