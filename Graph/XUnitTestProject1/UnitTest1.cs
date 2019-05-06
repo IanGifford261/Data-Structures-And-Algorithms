6615f31fef4cf6f193b9eb6987b1dc7b0e988b55
iangifford <lightyagamie10@gmail.com>
2019-05-06 12:00:21 -0700

    push

--- Graph/XUnitTestProject1/UnitTest1.cs
+++ Graph/XUnitTestProject1/UnitTest1.cs
@@ -12,5 +12,21 @@ namespace XUnitTestProject1
             Vertex<string> vertex = new Vertex<string>("Node1");
             Assert.Equal("Node1", vertex.Value);
         }
+
+        [Fact]
+        public void CanCreateEdge()
+        {
+            Vertex<string> vertex = new Vertex<string>("Node1");
+            Edge<string> edge = new Edge<string>();
+            edge.Vertex = vertex;
+            Assert.Equal("Node1", edge.Vertex.Value.ToString());
+        }
+
+        //[Fact]
+        //public void CanRetrieveNodes()
+        //{
+        //    Graph<string> graph = new Graph<string>();
+        //    Assert.IsType(Graph<string>(graph);
+        //}
     }
 }
